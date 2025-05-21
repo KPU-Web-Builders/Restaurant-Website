f4609f680a422dc0f4929cf95251e73f9e259d89
Murtaza Rahimi <murtaza2003rahimi@gmail.com>
2025-05-21 21:05:42 +0430

    The header Completed

--- src/Navbar/header.js
+++ src/Navbar/header.js
@@ -2,10 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Creating a sidebar in javaScript 
   const smallScreenHeader = `
-  <aside class=" text-2xl transition-all duration-500 ease-in-out flex flex-col items-center justify-between md:text-4xl    bg-[#02eb02] w-full h-full pt-10 pb-8 text-white font-semibold font-serif " id="sideBar">
+  <aside class=" text-2xl transition-all duration-500 ease-in-out flex flex-col items-center justify-between md:text-4xl  bg-[#02eb02] w-full h-full pt-10 pb-8 text-white font-semibold font-serif md:hidden lg:hidden" id="sideBar">
 
     <!-- close Icon -->
-     <button class="absolute top-2 right-3.5 cursor-pointer  text-2xl" class="close">✖</button>
+     <button class="absolute top-2 right-3.5 cursor-pointer  text-2xl" class="close" onClick = 'handleClick()'>✖</button>
 
     <!-- Logo Or Name -->
      <span>Restuarnat <sup>Name</sup></span>
@@ -23,8 +23,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // Creating a navbar in javaScript 
   const largeScreenHeader = `
     <!-- Open Icon -->
-  <button class="absolute text-black cursor-pointer top-2 right-3.5 text-2xl hidden" id="open">✔</button>
-  <nav class="hidden transition-all duration-500 ease-in flex md:flex bg-[#02eb02]  w-full h-40 pl-8 pr-8 md:gap-2.5 items-center justify-between font-semibold font-serif md:text-3xl text-white  md:hidden">
+  
+  <nav class="hidden transition-all duration-500 ease-in md:flex bg-[#02eb02]  w-full h-40 pl-8 pr-8 md:gap-2.5 items-center justify-between font-semibold font-serif md:text-3xl text-white  ">
     <!-- Logo Or Name -->
     <span class="md:text-xl">Restaurant <sup class="">Name</sup></span>
     <!-- Link to the Pages -->
@@ -46,21 +46,19 @@ document.addEventListener("DOMContentLoaded", () => {
     smallScreenHeader;
 });
 
-const openBtn =   document.querySelector("#open");
-const closeBtn = document.querySelector(".close");
-const sideBar = document.getElementById("sideBar");
-console.log(closeBtn);
-console.log(sideBar);
+const openBtn =   document.getElementById("open");
+const SmallScreen = document.getElementById("smallScreensidbarLoader");
 
-//adding event to the close button
-// closeBtn.addEventListener("click", function(){
-//   sideBar.classList.add("hidden");
-//   openBtn.classList.remove("hidden");
-// });
+function handleClick(){
+    SmallScreen.style.display = "none";
+    openBtn.classList.remove("hidden");
+    openBtn.classList.add("flex");
+}
+openBtn.addEventListener("click", ()=>{
+  SmallScreen.style.display = "flex";
+   openBtn.classList.remove("flex");
+    openBtn.classList.add("hidden");
+})
 
-//adding event to the open button
-// openBtn.addEventListener("click", ()=>{
-//   sideBar.classList.remove("hidden");
-//   openBtn.classList.add("hidden");
-// })
+
 
